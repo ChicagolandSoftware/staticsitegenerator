9c14ec005b028f02d482f8a3e713a1ca9e6e91d8
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-01-17 22:19:21 -0600

    sort of fixed scoping issue but not 100% -- saving mid-way in case I make it worse I am so done with programming right now oh my goodness

--- generator.py
+++ generator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Alan's Static Site Generator
 # Generates static websites in the projects subfolder
 # copy the contents of your project's website_files folder to
@@ -5,48 +7,37 @@
 # or git init in the website_files folder for GitHub Pages
 # For more documentation, see the repo:
 # https://github.com/0x416c616e/staticsitegenerator
-#!/usr/bin/env python3
 import time
 import os
 import sys
 import json
 
+
 working_project = ""  # the project that is open
 
 # main program entry point
 def main():
+
     args_provided = has_command_line_args()
-    fast_mode = True # turn this off later, the text prompt is useful for new users but annoying for
-                     # me as the developer because it makes running the program take longer
+    print("args_provided:")
+    print(args_provided)
+    fast_mode = True  # turn this off later, the text prompt is useful for new users but annoying for
+                      # me as the developer because it makes running the program take longer
+
     # what it does at first depends on if there are any cli args/options or not
     # no args = text menu prompts telling you what you can do, more beginner-friendly
     # cli args = faster but for more advanced users
+    scoping_hotfix = ""  # bad workaround
     if args_provided:
-        print("handle the command line arguments here, at least for opening or making a new project")
-        # opening by cli args i.e. python generator.py --open some_project
-        if ((sys.argv[1] == "--open") or (sys.argv[1] == "-o")) and (len(sys.argv) == 3):
-            arg_name = sys.argv[2] # name of project provided as an argument
-            print("you are trying to open an existing project with name: " + arg_name)
-            open_project(arg_name)
-        # making a new project via cli args .e. python generator.py --new new_project
-        elif ((sys.argv[1] == "--new") or (sys.argv[1] == "-n")) and (len(sys.argv) == 3):
-            arg_name = sys.argv[2]
-            print("you are trying to create a new project with name: " + arg_name)
-            create_project(arg_name)
-        else:
-            print("add more elifs in the future for different command line args")
-            print("if you got here it means you provided invalid command line args")
-            print("sys.argv[1]: " + str(sys.argv[1]))
-            print("sys.argv[2]: " + str(sys.argv[2]))
-            print("length of sys.argv:" + str(len(sys.argv)))
-            sys.exit()
+        scoping_hotfix = arg_steps();
     # end of cli args stuff
 
     # beginning of program entry for no when generator.py is run with no arguments
     if not fast_mode:
         beginning_prompt()  # simple text notice
     # MAIN START
-    menu_check_thing(args_provided)
+    print("REEEE right here scoping_hotfix is " + scoping_hotfix)
+    menu_check_thing(args_provided, scoping_hotfix)
 
 
 # just gives info to the user, sleeps to make them look at it longer
@@ -58,30 +49,68 @@ def beginning_prompt():
     print("Only quit with the quit command.")
     time.sleep(5)
 
+def arg_steps():
+    print("handle the command line arguments here, at least for opening or making a new project")
+    # opening by cli args i.e. python generator.py --open some_project
+    if ((sys.argv[1] == "--open") or (sys.argv[1] == "-o")) and (len(sys.argv) == 3):
+        working_project = sys.argv[2]
+        arg_name = sys.argv[2]  # name of project provided as an argument
+        print("you are trying to open an existing project with name: " + arg_name)
+        open_project(arg_name)
+    # making a new project via cli args .e. python generator.py --new new_project
+    elif ((sys.argv[1] == "--new") or (sys.argv[1] == "-n")) and (len(sys.argv) == 3):
+        working_project = sys.argv[2]
+        arg_name = sys.argv[2]
+        print("you are trying to create a new project with name: " + arg_name)
+        create_project(arg_name)
+    else:
+        print("add more elifs in the future for different command line args")
+        print("if you got here it means you provided invalid command line args")
+        print("sys.argv[1]: " + str(sys.argv[1]))
+        print("sys.argv[2]: " + str(sys.argv[2]))
+        print("length of sys.argv:" + str(len(sys.argv)))
+        sys.exit()
+    print("REEEEE working project is" + working_project)
+    return working_project
+
 # attempt to open a project, but checks if it's valid first
 def open_project(project_name):
-    print("not finished yet")
+    # enter validity check here
+    project_name_is_valid = True  # stub
+    print("TODO: regex/input validation, store in project_name_is_valid")
     # just check that the project is valid, then eventually proceed to main_project_menu
     proceed = True
     working_project = project_name
+    return project_name_is_valid
 
 # attempt to create a project, check if name is valid and not in use
 def create_project(project_name):
-    print("not finished yet")
+    # enter validity check here
+    project_name_is_valid = True  # stub
+    print("TODO: regex/input validation, store in project_name_is_valid")
     # make new project by copying from template project folder, then proceed to main_project_menu
     proceed = True
     working_project = project_name
+    return project_name_is_valid
+
 
 # check if program is being run with command line args
 def has_command_line_args():
     if len(sys.argv) > 1:
         return True
+    else:
+        return False
 
-# the bulk of the program's options
+
+# this is run before proceeding to the main_project_menu, which is
+# where everything useful happens
+# not gonna lie, this could be a lot cleaner, but it works
 # in the future I want to replace this function with a GUI
 # be sure to make things modular in here so I can hook them up to
 # GUI button event handlers instead of the input() and if/while loops
-def menu_check_thing(args_provided):
+def menu_check_thing(args_provided, current_working_project):
+    print("current working project: " + current_working_project)
+    # at this point, the project may or may not be set yet
     menu_choice = ""
     if not args_provided:
         print("this is the menu you get when you didn't provide any extra command line args")
@@ -93,24 +122,27 @@ def menu_check_thing(args_provided):
     proceed = args_provided # whether to skip the file open or creation
     # proceed means to skip the process of selecting whether to open a project or make a new one
     # if you did the command line args then you already opened something
+    # kinda spaghetti-ish but whatever, it works
 
     while menu_choice != 'quit' and not proceed: # if args were provided then this does not happen
         if menu_choice == str(1):
             project_name = input("Enter the project name to open: ")
-            print("STUB: check if the folder exists, and if not, then loop and try again")
-            print("STUB: check that the project name is not blank")
             print("Opening existing project called " + project_name + ".")
-            open_project(project_name)
-            proceed = True
+            project_success = open_project(project_name)  # returns whether it succeeded or not
+            if not project_success:
+                print("invalid project to open, try again")
+            else:
+                proceed = True
         elif menu_choice == str(2):
             project_name = input("Enter a name for the new project: ")
-            print("STUB: check that the project name is not blank")
-            print("STUB: check that the project does not already exist (don't want to overwrite)")
             print("Creating a new project called " + project_name + " in the projects folder.")
             print("STUB: create the files and folders for the new project")
             print("STUB: get the username, about text, website title, email, twitter, and github from the user")
-            create_project(project_name)
-            proceed = True
+            project_success = create_project(project_name)  # returns whether it succeeded or not
+            if not project_success:
+                print("invalid project name, try again")
+            else:
+                proceed = True
         else:
             print("Invalid choice. Try again.")
             print("Here's what you can do with Alan's Static Site Generator:")
@@ -120,17 +152,24 @@ def menu_check_thing(args_provided):
     if not proceed:
         print("Goodbye.")
     else:
-        # proceeding with the projects menus
-        main_project_menu(working_project)
-
+        if not args_provided:
+            main_project_menu(project_name)
+        elif args_provided:
+            main_project_menu(working_project)
+        else:
+            print("what happened?")
 
 # you only get here after a project is open
 # if a new project is created, it is opened
 def main_project_menu(project_name):
+    working_project = project_name
+
     print("MAIN PROJECT MENU with open project " + project_name)
     print("Here are your choices: ")
-    print("1. articles")
+    print("1. article menu")
     print("2. work on this, it's not done")
+    print("DEBUG: value of project_name is " + project_name)
+    print("DEBUG: value of working_project is " + working_project)
     second_menu_choice = input("Type a number to do something, or type quit to quit: ")
     while second_menu_choice != 'quit':
         print("this is not finished yet")
@@ -142,8 +181,7 @@ def main_project_menu(project_name):
             print("Invalid choice. Try again.")
             print("MAIN PROJECT MENU with open project " + project_name)
             print("Here are your choices: ")
-            print("1. articles")
-            print("2. work on this, it's not done")
+            print("copy the entire numbered menu here again")
         second_menu_choice = input("Type a number to do something, or type quit to quit: ")
     print("Goodbye.")
 
