24593de6aafd209a3daa0b389983976cac01ce6f
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-01-23 01:57:47 -0600

    added basic info for what initial setup module will do, also testing the new .gitignore

---
+++ modules/initial_setup_module.py
@@ -0,0 +1,4 @@
+print("this is my highest priority right now, as of 1/23/2019")
+print("this will set up the project after it has been copied using shutil")
+print("after successfully opening a file, then you need to call functions from the initial setup module")
+print("but only if the ")
