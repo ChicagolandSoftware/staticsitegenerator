63759124d3edc44f5390dc3cca500681f3a13235
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-01-31 20:50:11 -0600

    added stub for project menu

--- modules/project_module.py
+++ modules/project_module.py
@@ -6,4 +6,45 @@ def project_menu():
     print("5. Regenerate static HTML pages")
     print("7. Return to main menu")
     print("THIS IS NOT COMPLETE")
-    #input()  # get rid of this later, this is only a placeholder to stop it from proceeding immediately
+
+
+def view_project_info(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("view project info goes here (stub)")
+
+
+def rename_project(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("rename project goes here (stub)")
+
+
+def delete_project(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("delete project goes here (stub)")
+
+
+def open_a_different_project(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("open a different project goes here (stub)")
+
+
+def regenerate(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("regenerate goes here (stub)")
+
+
+def return_to_main_menu(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("Returning to main menu.")
+
+
+def clear_and_prompt(project_object):
+    input("Press enter to continue.")
+    project_object.clear_terminal()
+    project_object.top_prompt(project_object.get_project())
