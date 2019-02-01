46668681313ecaa0f31685dbf9942e5b203987c0
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-01-31 20:22:05 -0600

    added stub for settings menu

--- modules/settings_module.py
+++ modules/settings_module.py
@@ -4,5 +4,40 @@ def settings_menu():
     print("3. Website title menu")
     print("4. Logo menu")
     print("5. Return to main menu")
-    print("THIS IS NOT COMPLETE")
-    #input()  # get rid of this later, this is only a placeholder to stop it from proceeding immediately
+
+
+def about_page_menu(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("about page menu goes here (stub)")
+
+
+def social_media_menu(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("social media menu goes here (stub)")
+
+
+def website_title_menu(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("website title menu goes here (stub)")
+
+
+def logo_menu(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("logo menu goes here (stub)")
+
+
+def return_to_main_menu(project_object):
+    project_object.clear_terminal()
+    project_object.sub_prompt(project_object.get_project())
+    print("Returning to main menu.")
+
+
+def clear_and_prompt(project_object):
+    input("Press enter to continue.")
+    project_object.clear_terminal()
+    project_object.top_prompt(project_object.get_project())
+
